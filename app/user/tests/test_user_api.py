8c807ee51ca7ee1750f7826e6cc711949dbd77f9
Junya <junyasewai@gmail.com>
2020-01-18 17:30:39 -0500

    Added User create view

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,16 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
 from rest_framework.test import APIClient
 from rest_framework import status
 
 
-CREATE_USER_URL = reverse('user:core')
+CREATE_USER_URL = reverse('user:create')
 
 
 def create_user(**param):
-    return get_user_model().objects.create_user(**params)
+    return get_user_model().objects.create_user(**param)
 
 
 class PublicUserApiTests(TestCase):
@@ -28,7 +27,8 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_201_CREATEd)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        print(res.data)
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
